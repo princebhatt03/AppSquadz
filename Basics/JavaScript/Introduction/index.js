f3272129be6dc4710335da0753d93c28138dbae2
Prince Bhatt <sharmaprince89667@gmail.com>
2025-09-05 10:39:15 +0530

    AppSquadz Repo Created

---
+++ Basics/JavaScript/Introduction/index.js
@@ -0,0 +1,22 @@
+console.log('Prince Bhatt');
+console.log('JavaScript');
+
+// JavaScript is a Single Threaded and interpreted Programming language basically used for developing or creating Dynamic and interactive web pages, it is used for both client side and server side scripting
+
+// Javascript is a Object-Oriented, Single Threaded, Interpreted, Event Driven, Asynchronous, Platform Independent, Capable of exception handling, Alows Anonymous Functions
+
+/*JavaScript's flexibility extends to both the client-side and server-side, allowing developers to create complete web applications. Here’s how it functions in each environment:
+
+Client-Side:
+
+Involves controlling the browser and its DOM (Document Object Model).
+Handles user events like clicks and form inputs.
+Common libraries include AngularJS, ReactJS, and VueJS.
+
+Server-Side:
+
+Involves interacting with databases, manipulating files, and generating responses.
+Node.js and frameworks like Express.js are widely used for server-side JavaScript, enabling full-stack development. */
+
+// For running the JavaScript for Server side we need to install Node.js from nodejs.org
+// Nodejs is Basically a JavaScript runtime built on Chrome's V8 JavaScript engine that allows you to run JavaScript code outside of a web browser, enabling server-side scripting and the development of scalable network applications.
